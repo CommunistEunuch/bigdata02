7cd3c9fc04de31da0ed0bb4b050f43476477ddd9
medicaless <danil00@naver.com>
2025-09-04 15:20:25 +0900

    week02_numpy.py update

--- week02/week02_numpy.py
+++ week02/week02_numpy.py
@@ -4,9 +4,37 @@
 
 import numpy as np
 
-list1 = [1, 2, 3, 4, 5]
-array1 = np.array(list1)
-print(list1, type(list1))
-print(array1, type(array1))
-array2 = np.array([[1, 2, 3] ,[4, 5, 6]])
-print(array2, type(array2))
+#zeros 함수
+#zeros = np.zeros((3,4))
+#zeros = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0]], dtype=float)
+
+
+#다 채워서 넣는 방법
+#ones = np.ones((3,5))
+#ones = np.array([[1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1]], dtype=float)
+
+#print(zeros)
+#print(ones)
+
+
+#구간 배열 (증감식)(array range)
+#0에서 20까지 2씩 증가
+#range_array = np.arange(0, 20, 2)
+
+#1에서 10까지 1씩 증가
+#range_array = np.arange(1, 10)
+
+#range_array = list()
+#for i in range(1, 10):
+#    range_array.append(i)
+
+#print(range_array)
+
+
+#구간 설정 2번째
+#space_array = np.linspace(0, 1, 5) # 0부터 1까지 숫자 5개로 구간 설정
+#print(space_array)
+
+#space_array = np.linspace(0, 100, 5) # 0부터 100까지 숫자 5개로 구간 설정
+#print(space_array)
+
