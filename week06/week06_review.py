f264f81d4bd9ff8a848c2c9079f92113ac97e567
medicaless <danil00@naver.com>
2025-10-02 15:52:01 +0900

    week06 pandas modified

--- week06/week06_review.py
+++ week06/week06_review.py
@@ -76,14 +76,38 @@ print(df.describe()) #수치형 데이터 요약 통계
 # plt.show()#그리기
 
 #카테고리 플롯
-plt.figure(figsize=(8,4))#그래프 해상도 설정
-sns.catplot(#남자, 여자로 나누어 class로 order순서 / 1등석 2등석 3등석 생존여부
-    data=df,
-    x="class",
-    hue="survived",
-    col="sex",
-    kind="count",
-    order=["First", "Second", "Third"]
-)
-#group by와 비슷함
-plt.show()#그리기
+# plt.figure(figsize=(8,4))#그래프 해상도 설정
+# sns.catplot(#남자, 여자로 나누어 class로 order순서 / 1등석 2등석 3등석 생존여부
+#     data=df,
+#     x="class",
+#     hue="survived",
+#     col="sex",
+#     kind="count",
+#     order=["First", "Second", "Third"]
+# )
+# #group by와 비슷함
+# plt.show()#그리기
+
+#--------------------------------------------------
+# #df =sns.load_dataset('titanic') //dataFrame이요~
+# print(df.groupby('survived').describe()) #판다스를 활용한 groupBy //Test
+#
+#
+# print(df.groupby('sex')['survived'].describe()) #판다스를 활용한 groupBy //아래와 비교 (통계)
+# print(df.groupby('sex')['survived'].mean()) #판다스를 활용한 groupBy //아래와 비교 (평균)
+#
+# #성별에 따른 생존자 //판다스를 시각화 한다면 아래와 같음.
+# plt.figure(figsize=(8,4))#그래프 해상도 설정
+# sns.countplot(data=df, x="sex", hue="survived") #x축 y축 설정 (survived의 값으로 나누기)
+# plt.title("Survival Status by Survival")
+# plt.ylabel('Count') #y축
+# plt.xlabel('Sex') #x축
+# plt.legend(loc='upper right',title="Survived", labels=['no', 'yes'])
+# plt.show()#그리기
+
+
+#aggregation 성별로 그룹을 묶어서 탑습 등급을 나누어 평규, 인원수 보여주는
+#print(df.groupby(['sex','class'])['survived'].agg(['mean','count']))
+#print(df.groupby(['sex','class'])['survived'].agg(['median','count'])) #중간값이 있냐 없냐
+#print(df.groupby(['sex','class'])['survived'].agg(['min','max'])) #살았냐 죽었냐
+#print(df.groupby(['sex','class'])['survived'].agg(['min','count'])) #집계 함수
