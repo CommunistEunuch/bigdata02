3b3784eb578d4c8814a36afefe0c2d7ca6718e2b
medicaless <danil00@naver.com>
2025-10-23 16:24:58 +0900

    week09 duration added

--- week09/week09_pandas.py
+++ week09/week09_pandas.py
@@ -18,6 +18,58 @@ uber = pd.read_csv("Uber.csv")
 #print(uber.tail()) #여기서 맨 마지막 행이 총 합을 적어놓은 것이라고 알 수 있음
 
 #특정 행 값을 날려버리는 방법
-uber = uber.drop(1155)
-print(uber.tail())
-print(uber.describe())
+#uber = uber.drop(1155)
+#print(uber.tail())
+#print(uber.describe())
+
+########################################################
+
+#에러를 처리하는 방법(datetime 안에 있는) 날짜 시간으로 데이터 변환
+#uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'])
+#print(uber)
+
+#error='coerce'를 쓰면 에러를 처리할 수 있다. (각 행에서 보이도록)
+uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'], errors='coerce')
+uber['END_DATE*'] = pd.to_datetime(uber['END_DATE*'], errors='coerce')
+#print(uber)
+
+uber = uber.sort_values(['START_DATE*', 'END_DATE*'])
+#print(uber)
+
+#따라서 Dtype도 바뀐다.
+#print(uber.info())
+
+#곂치는거 솎아내기
+#print(uber['START_DATE*'].unique())
+#print(uber['END_DATE*'].unique())
+
+
+#활용 -->
+#uber = uber.drop(1155)
+#print(uber['END_DATE*'].unique())
+#print(uber[uber['MILES*'] == uber['MILES*'].max()])
+
+
+#_#_#_#_#_#_#_#_#
+
+#이용 사유 보기
+#print(uber['PURPOSE*'].value_counts())
+
+#이동 사유가 없는 것 보기
+#print(uber['PURPOSE*'].isna().sum())
+
+#새로운 컬럼 만들기 (걸린 시간)
+uber['DURATION*'] = (uber['END_DATE*'] - uber['START_DATE*'])
+#print(uber['CATEGORY*'].value_counts())
+
+# VVVVV 활용하는 방법 VVVVV
+#print(uber.groupby(['CATEGORY*', 'PURPOSE*'])[['MILES*']].agg(['mean', 'std'])) #산술 평균, 표준 편차
+
+#print(uber[uber['PIRPOSE*'] == 'Airport/Travel']['MILES*'])
+
+#print(uber.groupby(['CATEGORY*', 'PURPOSE*'])[['MILES*','DURATION*']].agg(['mean', 'median', 'std'])) #산술 평균, 표준 편차
+
+#print(uber[uber['PURPOSE*'] == 'Airport/Travel'][['MILES*', 'DURATION*']])
+
+
+
