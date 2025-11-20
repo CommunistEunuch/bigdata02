03d3ad83550675c064fca43f8af27532c08cd77c
medicaless <danil00@naver.com>
2025-11-20 16:02:16 +0900

    week13 research

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -37,3 +37,31 @@ apple['Volatility'] = apple['Daily_Return'].rolling(window=20).std()
 # print(apple.info())
 apple = apple.reset_index()
 print(apple[['Date', 'Close', 'Daily_Return', 'MA_20', 'Volatility']].tail(10))
+
+# print(apple[['Date', 'Close', 'Daily_Return', 'MA_20', 'Volatility']].tail(10))
+apple['Year'] = apple['Date'].dt.year
+yearly_stats = apple.groupby('Year')['Close'].agg([
+    ('최고', 'max'),
+    ('최저', 'min'),
+    ('평균', 'mean'),
+    ('개수', 'count'),
+])
+# print(yearly_stats.tail(9))
+
+apple['Month'] = apple['Date'].dt.month
+monthly_stats = apple.groupby('Month')['Close'].mean()
+# print(monthly_stats)
+
+# 분기별 수익율 분석
+apple['Quarter'] = apple['Date'].dt.quarter
+quarterly_return = apple.groupby(['Year', 'Quarter'])['Daily_Return'].sum()
+# print(quarterly_return)
+
+# 거래량이 많은 날 분석
+# print(apple['Volume'])
+apple['Volume_Category'] = pd.cut(apple['Volume'],
+                                  bins=[0, 100000000, 300_000_000, 5e8, 10e10],
+                                  labels=['Low', 'Medium', 'High', 'Very High']
+                                  )
+volume_analysis = apple.groupby(['Volume_Category'])['Close'].agg(['count', 'mean'])
+print(volume_analysis)
